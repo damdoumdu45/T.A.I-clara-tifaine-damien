bcd69e51066c1cb60082c5cffc6400ff65f49562
damdoumdu45 <damien.hoffmann@etu.estia.fr>
2023-02-14 11:06:36 +0100

    connexion BDD

--- src/AccueilControleur.java
+++ src/AccueilControleur.java
@@ -17,7 +17,7 @@ public class AccueilControleur extends HttpServlet {
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public AccueilControleurssss() {
+    public AccueilControleur() {
         super();
         // TODO Auto-generated constructor stub
     }
