5c6017fc0febdad48e33ec0966e6a4b5328faa54
clarag81 <clara.gesset@etu.estia.fr>
2023-02-17 13:41:03 +0100

    creation fichier bean modele pour table historique, inventaire et produit

--- src/AjouterBonEntreeDAOModele.java
+++ src/AjouterBonEntreeDAOModele.java
@@ -1,4 +1,10 @@
+import java.sql.Connection;
 
 public class AjouterBonEntreeDAOModele {
+	
+	public AjouterBonEntreeBDDModele()
+	{
+		
+	}
 
 }
---
+++ src/InventaireBeanModele.java
@@ -0,0 +1,27 @@
+
+public class InventaireBeanModele {
+	private int id;
+	private String date;
+	
+	public InventaireBeanModele() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+
+}
